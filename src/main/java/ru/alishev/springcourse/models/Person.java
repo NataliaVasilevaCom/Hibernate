d1eae5a2ea2a7547b04d479730402ced43590198
Natalia <81523437+NataliaVasilevaCom@users.noreply.github.com>
2022-12-08 15:42:51 +0500

    Update Person.java

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -91,3 +91,25 @@ public class Person {
     }
 
 }
+///////////PROJECT2: new directory in models App
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getFullName());
+            System.out.println(person.getYearOfBirth());
+
+            session.getTransaction.commit();
+            
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
