957bebf3019c49bc2d25ab954b5146ad6ec63f59
Natalia <81523437+NataliaVasilevaCom@users.noreply.github.com>
2022-12-27 15:57:30 +0500

    Update SpringConfig.java

--- src/main/java/ru/alishev/springcourse/config/SpringConfig.java
+++ src/main/java/ru/alishev/springcourse/config/SpringConfig.java
@@ -72,15 +72,41 @@ public class SpringConfig implements WebMvcConfigurer {
                 new DriverManagerDataSource();
         dataSource.setDriverClassName(env.getRequiredProperty("hibernate.driver_class")));
         dataSource.setUrl(env.getRequiredProperty("hibernate.connection.url"));
-        dataSource.setUsername(environment.getProperty("username_value"));
-        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setUsername(env.getRequiredProperty("hibernate.connection.username"));
+        dataSource.setPassword(env.getRequiredProperty("hibernate.connection.password"));
 
         return dataSource;
     }
-
-    @Bean
+    
+    //переходим на Hibernate, JdbcTemplate не нужен
+    /*@Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
+    }*/
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+
+        return properties;
     }
 
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("ru.alishev.springcourse.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+
+        return transactionManager;
+    }
 }
