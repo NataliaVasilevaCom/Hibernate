7950600982611c8b921063717696f4a96fa5d1e0
Natalia <81523437+NataliaVasilevaCom@users.noreply.github.com>
2022-12-28 09:26:29 +0500

    Update PersonDAO.java

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -21,37 +21,38 @@ import java.util.Optional;
 @Component
 public class PersonDAO {
     
-
+    private final SessionFactory sessionFactory;
+    
     @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
-
+    
+    @Transactional
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+        Session session = sessionFactory.getCurrentSession();
+        //обычный hibernate код
+        return null;
     }
 
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
-                .stream().findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person(full_name, year_of_birth) VALUES(?, ?)", person.getFullName(),
-                person.getYearOfBirth());
+        
     }
 
 
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET full_name=?, year_of_birth=? WHERE id=?", updatedPerson.getFullName(),
-                updatedPerson.getYearOfBirth(), id);
+        
     }
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+        
 
     }
 
-    public Optional<Person> getPersonByFullName(String fullName) {
+    /*public Optional<Person> getPersonByFullName(String fullName) {
         return jdbcTemplate.query("SELECT * FROM Person WHERE full_name=?", new Object[]{fullName},
         new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
     }
@@ -59,7 +60,7 @@ public class PersonDAO {
     public List<Book> getBooksByPerson(int id) {
         return jdbcTemplate.query("SELECT * FROM Book WHERE person_id=?", new Object[]{id},
         new BeanPropertyRowMapper<>(Book.class));
-    }
+    }*/
 /////////////////test batch update
 
     /*public void testMultipleUpdate() {
