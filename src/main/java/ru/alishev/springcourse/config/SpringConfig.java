c49a3ab56ccfef32d5e593a6d136f904e2f1dcb1
Natalia <81523437+NataliaVasilevaCom@users.noreply.github.com>
2023-01-09 14:39:28 +0500

    Update SpringConfig.java

--- src/main/java/ru/alishev/springcourse/config/SpringConfig.java
+++ src/main/java/ru/alishev/springcourse/config/SpringConfig.java
@@ -1,5 +1,74 @@
-/create new package repositories: ru.alishev.springcourse.repositories
+/*create new package repositories: ru.alishev.springcourse.repositories
+package ru.alishev.springcourse.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.alishev.springcourse.models.Person;
+
+/**
+ * @author Neil Alishev
+ */
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
+
+
+
+/create new package repositories: ru.alishev.springcourse.repositories
+package ru.alishev.springcourse.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.alishev.springcourse.models.Person;
+import ru.alishev.springcourse.repositories.PeopleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Neil Alishev
+ */
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> foundPerson = peopleRepository.findById(id);
+        return foundPerson.orElse(null);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        peopleRepository.save(person);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        updatedPerson.setId(id);
+        peopleRepository.save(updatedPerson);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        peopleRepository.deleteById(id);
+    }
+}
+*/
+    
+    
 package ru.alishev.springcourse.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
